823b04bfde5fb7d3dff2dc0807f3f60e6a267043
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-03-16 10:45:16 -0300

    feat: criando funcionalidade para clonar telefone

--- teste/index.ts
+++ teste/index.ts
@@ -1,27 +1,40 @@
 import Cliente from "../modelos/cliente";
 import Endereco from "../modelos/endereco";
-let cliente = new Cliente()
-cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
-cliente.nomeSocial = `Dom Pedro II`
-cliente.dataCadastro = new Date(1840, 6, 23)
-cliente.dataNascimento = new Date(1825, 11, 2)
-let endereco = new Endereco()
-endereco.rua = `R. do Catete`
-endereco.bairro = `Copacabana`
-endereco.cidade = `Rio de Janeiro`
-endereco.estado = `Rio de Janeiro`
-endereco.pais = `Brasil`
-endereco.codigoPostal = `22220-000`
-cliente.endereco = endereco
+import Telefone from "../modelos/telefone";
 
-let dependente = new Cliente()
-dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
-dependente.nomeSocial = `Princesa Isabel`
-dependente.dataCadastro = new Date(1921, 10, 14)
-dependente.dataNascimento = new Date(1846, 6, 29)
-dependente.endereco = (cliente.endereco.clonar() as Endereco)
-dependente.titular = cliente
-cliente.dependentes.push(dependente)
+let cliente = new Cliente();
+cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`;
+cliente.nomeSocial = `Dom Pedro II`;
+cliente.dataCadastro = new Date(1840, 6, 23);
+cliente.dataNascimento = new Date(1825, 11, 2);
+
+let endereco = new Endereco();
+endereco.rua = `R. do Catete`;
+endereco.bairro = `Copacabana`;
+endereco.cidade = `Rio de Janeiro`;
+endereco.estado = `Rio de Janeiro`;
+endereco.pais = `Brasil`;
+endereco.codigoPostal = `22220-000`;
+cliente.endereco = endereco;
+
+let telefone = new Telefone();
+telefone.ddd = "12";
+telefone.numero = "91111-1111";
+cliente.telefones.push(telefone);
+
+let dependente = new Cliente();
+dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`;
+dependente.nomeSocial = `Princesa Isabel`;
+dependente.dataCadastro = new Date(1921, 10, 14);
+dependente.dataNascimento = new Date(1846, 6, 29);
+dependente.endereco = cliente.endereco.clonar() as Endereco;
+
+cliente.telefones.forEach((telefone) => {
+  dependente.telefones.push(telefone.clonar() as Telefone);
+});
+
+dependente.titular = cliente;
+cliente.dependentes.push(dependente);
 
 console.log(cliente);
 console.log(dependente);
